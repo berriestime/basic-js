b01344a5b20c78f8352c7daac7497e12ccf4140c
Clukva <berries.whale@gmail.com>
2023-12-16 03:55:43 +0300

    feat: add what-season function

--- src/what-season.js
+++ src/what-season.js
@@ -11,50 +11,48 @@ const { NotImplementedError } = require("../extensions/index.js");
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/*date*/) {
-  throw new NotImplementedError("Not implemented");
-  // Check if the provided date is an instance of Date and is valid
-  // if (!(date instanceof Date) || isNaN(date)) {
-  //   throw new Error("Invalid date!");
-  // }
+function getSeason(date) {
+  if (!(date instanceof Date) || isNaN(date)) {
+    throw new Error("Invalid date!");
+  }
 
   // Check if the date object properties have been tampered with
-  // const methods = ["toString", "toDateString", "valueOf", "getTime"];
-  // for (let method of methods) {
-  //   if (
-  //     typeof date[method] !== "function" ||
-  //     date[method]() !== Date.prototype[method].call(date)
-  //   ) {
-  //     throw new Error("Invalid date!");
-  //   }
-  // }
+  const methods = ["toString", "toDateString", "valueOf", "getTime"];
+  for (let method of methods) {
+    if (
+      typeof date[method] !== "function" ||
+      date[method]() !== Date.prototype[method].call(date)
+    ) {
+      throw new Error("Invalid date!");
+    }
+  }
 
   // Check for the toStringTag to be 'Date'
-  // if (Object.prototype.toString.call(date) !== "[object Date]") {
-  //   throw new Error("Invalid date!");
-  // }
+  if (Object.prototype.toString.call(date) !== "[object Date]") {
+    throw new Error("Invalid date!");
+  }
 
-  // const month = date.getMonth();
-  // switch (month) {
-  //   case 0:
-  //   case 1:
-  //   case 11:
-  //     return "winter";
-  //   case 2:
-  //   case 3:
-  //   case 4:
-  //     return "spring";
-  //   case 5:
-  //   case 6:
-  //   case 7:
-  //     return "summer";
-  //   case 8:
-  //   case 9:
-  //   case 10:
-  //     return "autumn";
-  //   default:
-  //     throw new Error("Invalid date");
-  // }
+  const month = date.getMonth();
+  switch (month) {
+    case 0:
+    case 1:
+    case 11:
+      return "winter";
+    case 2:
+    case 3:
+    case 4:
+      return "spring";
+    case 5:
+    case 6:
+    case 7:
+      return "summer";
+    case 8:
+    case 9:
+    case 10:
+      return "autumn";
+    default:
+      throw new Error("Invalid date");
+  }
 }
 
 module.exports = {
