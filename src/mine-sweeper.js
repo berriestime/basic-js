9a4fa512fbad13a9344bf6f7ab8d6ac1e24dff80
Clukva <berries.whale@gmail.com>
2023-12-19 22:29:10 +0300

    feat: add mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function countMines(matrix, row, col) {
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+  let count = 0;
+
+  for (let i = Math.max(0, row - 1); i <= Math.min(row + 1, numRows - 1); i++) {
+    for (
+      let j = Math.max(0, col - 1);
+      j <= Math.min(col + 1, numCols - 1);
+      j++
+    ) {
+      if (i !== row || j !== col) {
+        count += matrix[i][j] ? 1 : 0;
+      }
+    }
+  }
+
+  return count;
+}
+
+function minesweeper(matrix) {
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    result.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      result[i].push(countMines(matrix, i, j));
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
